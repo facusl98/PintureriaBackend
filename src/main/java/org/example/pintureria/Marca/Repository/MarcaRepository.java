6d62d94d9396442ad5adc3ac054aaf385bc4902d
Facundo Castro <84674742+facusl98@users.noreply.github.com>
2026-08-25 11:59:13 -0300

    Delete src/main/java/org/example/pintureria directory

--- src/main/java/org/example/pintureria/Marca/Repository/MarcaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.pintureria.Marca.Repository;
-
-import org.example.pintureria.Marca.Entity.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-}
--- src/main/java/org/example/pintureria/Producto/Repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.pintureria.Producto.Repository;
-
-import org.example.pintureria.Producto.Entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
