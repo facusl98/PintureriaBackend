72c3807b4f6d07438b8a317a8d86914862b14291
Facu <facucastrosl@gmail.com>
2026-08-26 18:55:57 -0300

    delete duplication, repair properties

--- src/main/java/com/example/pintureriadelsur/Producto/Controller/ProductoController.java
+++ src/main/java/com/example/pintureriadelsur/Producto/Controller/ProductoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/productos")
 public class ProductoController {
